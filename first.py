825c72e283918ec65b6bab7e1d97abb7092dea49
skrutg <artykov.syimyk.04@gmail.com>
2022-05-13 18:14:42 +0600

    first day

---
+++ first.py
@@ -0,0 +1,36 @@
+#4
+
+from pkg_resources import cleanup_resources
+
+
+a = 17*3 
+b = 12*5
+print(a<=b) 
+
+s = 12**3
+l = 13*7
+print(s>=l)
+
+m = 4**5
+r = 512 + 512
+print(m<=l)
+
+#1
+
+k = 17925
+n = 34**2
+print(k<=n,'k > n')
+
+q = 17925
+w = 26*3
+print(w<=q,'q > w')
+
+e = 17925
+i = 17*33
+print(i<=e,'i > e')
+ 
+ t = 17925
+ y = 4394*4
+ print(u<=y,'u > y')
+
+
